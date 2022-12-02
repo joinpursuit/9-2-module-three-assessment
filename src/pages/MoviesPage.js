f8ba088eb2e6106cf68d2d79c895a51eeb07f5e5
i-guerrero <isaacguerrero@pursuit.org>
2022-12-02 15:26:10 -0500

    Implement get movie details

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import MovieCard from "../Components/MovieCard";
 
 export default function MoviesPage() {
   const [movies, setMovies] = useState([]);
+  const [selectedMovieId, setSelectedMovieId] = useState("");
 
   function getMovies() {
     return fetch("films.json").then((response) => response.json());
+  }
+
+  function getSelectedMovieId(e) {
+    setSelectedMovieId(e.target.value);
   }
 
   useEffect(() => {
@@ -21,16 +27,23 @@ export default function MoviesPage() {
   return (
     <div className="movies">
       <h2>Select a Movie</h2>
-      <select name="movies" id="movie-select">
+      <select
+        name="movies"
+        id="movie-select"
+        onChange={(e) => getSelectedMovieId(e)}
+      >
         <option value=""></option>
         {movies.map((movie) => {
           return (
-            <option key={movie.id} value={movie.title}>
+            <option key={movie.id} value={movie.id}>
               {movie.title}
             </option>
           );
         })}
       </select>
+      {selectedMovieId ? (
+        <MovieCard movieId={selectedMovieId} movies={movies} />
+      ) : null}
     </div>
   );
 }
