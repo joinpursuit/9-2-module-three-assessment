949c24df5ead1fdac0385811ece669e48fdf4cad
Wwright91 <wisdomwright-martin@pursuit.org>
2022-12-02 09:52:28 -0500

    movie details completed

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,18 +6,57 @@ export default function Movies() {
 
     const [allMovies, setAllMovies] = useState([])
 
+    const [selectedMovie, setSelectedMovie] = useState("")
+
     useEffect(() => {
         fetch(films_json)
             .then(res => res.json())
         .then(setAllMovies)
 }, [])
 
-    console.log(allMovies)
+    // console.log(allMovies)
+
+    function handleChange(e) {
+        setSelectedMovie(e.target.value)
+    }
+
+    console.log(selectedMovie)
+
+    function filteredMovie() {
+        const chosenMovie = allMovies.filter((movie) => movie.title === selectedMovie)
+        // console.log("yes")
+        console.log(chosenMovie)
+
+        if (allMovies) {
+            return (
+                <div>
+                    {chosenMovie.map(movie => {
+                        return (
+                            <div key={movie.id}>
+                            <h2>
+                         Title: {movie.title}
+                    </h2>
+                    <p>Released Date: {movie.release_date }</p>
+                    <p>
+                       Description: {movie.description}
+                                </p>
+                            </div>
+                        )
+                    })}
+                    
+                   
+                </div>
+            )
+            
+        } else {
+            return null;
+        }
+    }
 
     return (
         <div className="movies">
             <h1>Select a Movie</h1>
-            <select>
+            <select onChange={handleChange}>
                 <option value=""></option>
                 {allMovies.map((movie) => {
                     return (
@@ -25,6 +64,14 @@ export default function Movies() {
                     )
                 })}
             </select>
+            <div>
+                <h2>
+                    {/* {selectedMovie} */}
+                   { filteredMovie()}
+                </h2>
+                
+            
+            </div>
        </div>
    ) 
 }
