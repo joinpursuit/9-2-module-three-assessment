40ab1c049e4e80dac1cf6b904bcb7a95599094ac
Karyn Huston <huston.karyn@gmail.com>
2022-12-02 10:05:34 -0500

    nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Movies from "./components/Movies";
+import People from "./components/People";
+import Locations from "./components/Locations";
+ 
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
