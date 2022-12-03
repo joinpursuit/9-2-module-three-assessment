e4681c134ee0214fbdb73d46c7bbe6bb323f2ea9
Bo Latt <bolattny@gmail.com>
2022-12-03 17:46:27 -0500

    add swiper to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,55 @@
+// Import Swiper React components
+import SwiperCore, { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-coverflow";
+
+import { Pagination, Navigation, EffectCoverflow } from "swiper";
+
+import { useFetch } from "../hooks/useFetch";
 export default function Home() {
+  const { data: movies } = useFetch("/films.json");
+  SwiperCore.use([Autoplay]);
+
   return (
     <div className="home">
       <h1>Welcome to Ghibli App </h1>
-
-      <img
-        // src="https://wallpaperaccess.com/full/6608528.jpg"
-        src="https://cdn.vox-cdn.com/thumbor/iCYHf3YSFCMYm3QvH0I9NOOfTa4=/0x0:1920x1038/1820x1213/filters:focal(1112x348:1418x654):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66852981/spirited_awaybr_disneyscreencaps.com_11321.0.jpg"
-        alt="ghibli logo"
-      />
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        speed={500}
+        effect={"coverflow"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, EffectCoverflow]}
+        className="mySwiper"
+      >
+        {movies &&
+          movies.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <img src={movie.movie_banner} alt="movie banner" />
+              <h2>{movie.title}</h2>
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 }
