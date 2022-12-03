8a2f0b7ee1c2c820ef3a7b330eb75f06b8f5c687
Bo Latt <bolattny@gmail.com>
2022-12-02 23:58:52 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import People from "./pages/People";
+import Locations from "./pages/Locations";
+
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <nav>
+      <Link to="/">
+        <img src="/logo.png" alt="studio ghilbi logo" />
+      </Link>
+      <Link to="/movies">Movies</Link>
+      <Link to="/people">People</Link>
+      <Link to="/Locations">Locations</Link>
+    </nav>
+  );
+}
