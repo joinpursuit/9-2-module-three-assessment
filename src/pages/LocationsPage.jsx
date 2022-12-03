5014fa86d2826cf40753bdc027000e8a56ec31a7
i-guerrero <isaacguerrero@pursuit.org>
2022-12-03 11:46:29 -0500

    Add sort locations by name or climate

--- src/pages/LocationsPage.jsx
+++ src/pages/LocationsPage.jsx
@@ -13,6 +13,39 @@ export default function LocationsPage() {
         return fetch("locations.json").then(response => response.json())
     }
 
+    function handleSortByName() {
+        const locationsCopy = [...locations];
+        locationsCopy.sort((locationA, locationB) => {
+            if (locationA.name > locationB.name) return 1
+            if (locationA.name < locationB.name) return -1
+            return 0
+        })
+        console.log(locationsCopy)
+        setLocations(locationsCopy);
+    }
+
+    function handleSortByClimate() {
+        const locationsCopy = [...locations];
+        locationsCopy.sort((locationA, locationB) => {
+            if (locationA.climate > locationB.climate) return 1
+            if (locationA.climate < locationB.climate) return -1
+            return 0
+        })
+        console.log(locationsCopy)
+        setLocations(locationsCopy);
+    }
+
+    function handleSortByTerrain() {
+        const locationsCopy = [...locations];
+        locationsCopy.sort((locationA, locationB) => {
+            if (locationA.terrain > locationB.terrain) return 1
+            if (locationA.terrain < locationB.terrain) return -1
+            return 0
+        })
+        console.log(locationsCopy)
+        setLocations(locationsCopy);
+    }
+
     useEffect(() => {
         getLocations().then(response => {
             setLocations(response)
@@ -23,6 +56,9 @@ export default function LocationsPage() {
     <div className='locations'>
         <h2>List of Locations</h2>
         <button onClick={handleClick}>{clicked ? "Hide Locations" : "Show Locations"}</button>
+        <button onClick={handleSortByName}>Sort by Name</button>
+        <button onClick={handleSortByClimate}>Sort by Climate</button>
+        <button onClick={handleSortByTerrain}>Sort by Terrain</button>
         <ul>
             {clicked ? locations.map(location => {
                 return (
