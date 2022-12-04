2abf32e0bd855d70e3024a9804cd3273acae3195
Karyn Huston <huston.karyn@gmail.com>
2022-12-04 00:24:28 -0500

    sorted by name

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -4,38 +4,59 @@ import { useState, useEffect } from "react";
 export default function Locations() {
   const [showMore, setShowMore] = useState(false);
   const [locations, setLocations] = useState([]);
-
+  const [sortedNames, setSortedNames] = useState();
+  const [toggleNames, setToggleNames] = useState(false);
   useEffect(() => {
     fetch("locations.json")
       .then((response) => response.json())
       .then(setLocations);
   }, []);
 
-  console.log(locations);
+  function sortLocationName() {
+    setToggleNames(!toggleNames);
+    const sortNames = [...locations].sort((a, b) => {
+      return a.name > b.name ? 1 : -1;
+    });
+    setSortedNames(sortNames);
+  }
+  // console.log(locations);
+  console.log(sortedNames);
   return (
     <div className="locations">
       <h1>Lis of Locations</h1>
       <button onClick={(e) => setShowMore(!showMore)}>
         {!showMore ? "Show Locations " : "Hide Locations"}
+      </button>{" "}
+      <button onClick={sortLocationName}>
+        {toggleNames ? "hide" : "Sort by Name"}
       </button>
-      {showMore && (
+      {showMore ? (
         <div className="locations">
-          <button>Sort By Name</button>
-          <button>Sort By Climate</button>
           <ul>
             {" "}
-            {locations.map((location) => {
-              return (
-                <li key={location.id}>
-                  <p>Name: {location.name}</p>
-                  <p>Climate: {location.climate}</p>
-                  <p>Terrain: {location.terrain}</p>
-                </li>
-              );
-            })}
+            {locations.map((location) => (
+              <li key={location.id}>
+                <p>Name: {location.name}</p>
+                <p>Climate: {location.climate}</p>
+                <p>Terrain: {location.terrain}</p>
+              </li>
+            ))}
           </ul>
         </div>
-      )}
+      ) : toggleNames ? (
+        <div className="locations">
+          <ul>
+            {" "}
+            {sortedNames.map((names) => (
+              <li key={names.id}>
+                <p>Name: {names.name}</p>
+                <p>Climate: {names.climate}</p>
+                <p>Terrain: {names.terrain}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,35 +6,35 @@ export default function Movies({ films }) {
 
   function handleChange(e) {
     e.preventDefault();
-   setUserSelection(e.target.value);
+    setUserSelection(e.target.value);
   }
 
-  let filterByTitle = films.filter((film) => film.title === userSelection)
+  let filterByTitle = films.filter((film) => film.title === userSelection);
 
- 
   return (
     <div className="movies">
       <h1>Select a Movie</h1>
       <select onChange={handleChange}>
         <option value=""></option>
-        {films.map((film) => {
-          return (
-            <option key={film.id} value={film.title}>
-              {film.title}
-            </option>
-          );
-        })}
+        {films.map((film) => (
+          <option key={film.id} value={film.title}>
+            {film.title}
+          </option>
+        ))}
       </select>
-      {filterByTitle.map((film) => {
-        return (
-          <aside>
-            <h3><strong>Title:</strong> {film.title}</h3>
-            <p><strong>Release Date:</strong> {film.release_date}</p>
-            <p><strong>Description:</strong> {film.description}</p>
-          </aside>
-        )
-      })
-    }
+      {filterByTitle.map((film) => (
+        <aside>
+          <h3>
+            <strong>Title:</strong> {film.title}
+          </h3>
+          <p>
+            <strong>Release Date:</strong> {film.release_date}
+          </p>
+          <p>
+            <strong>Description:</strong> {film.description}
+          </p>
+        </aside>
+      ))}
     </div>
   );
 }
