8300ab721f8264a0a38b74f6982ce3efd3fa33e7
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2022-12-04 14:05:50 -0500

    Add gif to home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1 className="splash-page">Welcome to GhibliApp</h1>;
+  return (
+    <div className="splash-page">
+      <h1>Welcome to GhibliApp</h1>
+      <aside>
+        <p>Thank you Raz and Steven for all your support!</p>
+        <img src="./splash-page.gif" alt="sunset" />
+      </aside>
+    </div>
+  );
 }
