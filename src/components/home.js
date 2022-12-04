299fcb331c2549fff28a5a2e9756749e74a0ce9f
cjs2408 <carlossuero@pursuit.org>
2022-12-04 15:55:39 -0500

    forgot the home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,7 @@
 export default function Home() {
     return (
-      <div></div>
+      <div style={{color:"red",display:"center",margin:"auto"}} 
+      className="movies" > <strong style={{display:"center",margin:"auto",fontSize:"100px" ,color:"red"}} 
+      className="movies">Welcome to GhibliApp</strong></div>
     )
   }
