2cec52624bdbce22f778d29e0954777e357a9809
SharukhGV <sharukhvelupillai@pursuit.org>
2022-12-03 20:24:56 -0500

    updated homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,29 @@
 function Home(){
 
 return(
-<div>
+<div className="homePAge">
 <h1>Welcome to Ghibli App</h1>
+<p>An Application built using the React framework, using the Studio Ghibli API's data imported to a JSON file directly in the applications Components folder. This is a frontend-only application that displays information about the movies, characters, and locations created by Studio Ghibli. </p>
+
+<div className="cardContact">
+<h3 >About the Developer</h3>
+
+<p><strong>Name:</strong> Sharukh G. Velupillai</p>
+<p><strong>Organization:</strong> PURSUIT</p>
+<p>
+        <strong>GitHub:</strong>
+        <a href="https://github.com/SharukhGV">
+Sharukh's GitHub Portfolio        </a>
+      </p><p>
+        <strong>Email:</strong>
+        <a href="mailto:sharukh.g.velupillai@gmail.com">
+          sharukh.g.velupillai@gmail.com
+        </a>
+      </p>
+</div>
+
+
+
 </div>
 
 )
