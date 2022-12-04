66b72f8faac09c8872667c7428ee35633a6d2afc
Karyn Huston <huston.karyn@gmail.com>
2022-12-04 02:21:38 -0500

    added gif to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function Home() {
-  return <h1>Welcome to GhibliApp</h1>;
+  return <div className="home">
+  <h1>Welcome to GhibliApp</h1> 
+  <img className="homeImg" src={require('/Users/karynhuston/module-three/9-2-module-three-assessment/src/2e3f29c15b812c074ea75e0893f784d8.gif')}/>
+  </div>;
 }
