2fa333a174daa19a78e2065deb42a213cf291a88
Karyn Huston <huston.karyn@gmail.com>
2022-12-04 01:26:34 -0500

    home image

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,21 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-
 export default function Nav() {
-
-    return(
-        <nav>
-        <Link to="/">
-            <img
-            src="public/studio-ghib-nav.jpeg"
-            />
-        </Link>
-        <Link to="/movies"><h3>Movies</h3></Link>
-        <Link to="/people"><h3>People</h3></Link>
-        <Link to="/locations"><h3>Locations</h3></Link>
-        </nav>
-    
-    );
+  return (
+    <nav>
+      <Link to="/">
+        <img src={require('/Users/karynhuston/module-three/9-2-module-three-assessment/src/studio-ghib-nav.jpeg')}/>
+      </Link>
+      <Link to="/movies">
+        <h3>Movies</h3>
+      </Link>
+      <Link to="/people">
+        <h3>People</h3>
+      </Link>
+      <Link to="/locations">
+        <h3>Locations</h3>
+      </Link>
+    </nav>
+  );
 }
